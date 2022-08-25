e72996082ac785ed382f18f0dd577295b090c4a2
Anish Chandgothia <90914492+anish2910@users.noreply.github.com>
2022-08-25 10:23:08 +0530

    Update virtualdestruct.cpp

--- virtualdestruct.cpp
+++ virtualdestruct.cpp
@@ -3,47 +3,28 @@
 using namespace std;
 
 class Base{
-
 public:
-
+ 
 Base()
-
 {
-
 cout<<"base con"<<endl;
-
 }
-
  virtual ~Base()
-
 {
-
 cout<<"base destructor"<<endl;
-
 }
-
 };
 
 class Derived:public Base{
-
 public:
-
 Derived()
-
 {
-
 cout<<"derived con"<<endl;
-
 }
-
 ~Derived()
-
 {
-
 cout<<"Derived destructor"<<endl;
-
 }
-
 };
 
 int main(){
