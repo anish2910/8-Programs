a6bbd23602feb5277b1b382ee4b7a41ee2b07546
Anish Chandgothia <90914492+anish2910@users.noreply.github.com>
2022-08-25 11:05:20 +0530

    Create Read Date & Time.cpp

---
+++ Date And Time/Read Date & Time.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<ctime>
+
+using namespace std;
+
+int main()
+{
+time_t tt;
+struct tm * ti;
+  
+time(&tt);//current calendar time
+ti=localtime(&tt);//points to tm containing local time
+
+cout<<"current day,date and time:  "<<asctime(ti);//converts the contents in the structure filled by localtime into human read version 
+
+return 0;
+
+}
