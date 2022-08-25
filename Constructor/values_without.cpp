b9a1ec27512a95dad201850a2f09fadf491b6c61
Anish Chandgothia <90914492+anish2910@users.noreply.github.com>
2022-08-25 10:58:19 +0530

    Create values_without.cpp

---
+++ Constructor/values_without.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// "student" is a class
+class Student {
+private: // private access specifier
+    int rollNo;
+    string stdName;
+    float perc;
+
+public: //public access specifier
+    //function to set the values
+    Student(){}
+    void setValue()
+    {
+        rollNo = 22;
+        stdName = "anish";
+        perc = 90;
+    }
+
+    //function to print the values
+    void printValue()
+    {
+        cout << "Student's Roll No.: " << rollNo << "\n";
+        cout << "Student's Name: " << stdName << "\n";
+        cout << "Student's Percentage: " << perc << "\n";
+    }
+
+};
+
+int main()
+{
+    // object creation
+    Student std;
+    //calling function
+    std.setValue();
+    std.printValue();
+    
+    return 0;
+
+}
