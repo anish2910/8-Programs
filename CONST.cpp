85b96f4710bf1648d69cc86f14f0d5987a80c912
Anish Chandgothia <90914492+anish2910@users.noreply.github.com>
2022-08-25 10:13:24 +0530

    Update CONST.cpp

--- CONST.cpp
+++ CONST.cpp
@@ -1,7 +1,5 @@
 //const variables
 
-
-
 /*#include<iostream>
 
 using namespace std;
@@ -21,9 +19,6 @@ cout<<"x:"<<x<<endl;
 return 0;
 
 }*/
-
-
-
 
 
 //const class data members
@@ -48,96 +43,50 @@ cout<<"value of i"<<i<<endl;
 
 };
 
-
-
 int main(){
-
 Test t;
-
 t.show(10);
-
 return 0;
-
 }
 
 
 
-
-
-
-
-
-
 /* #include<bits/stdc++.h>
-
 using namespace std;
-
 class Con 
-
 {
-
    int i=10;
-
    public:
-
    void show()const{
-
     i++;
-
     cout<<i<<endl;
-
    }                             //doesn't work
-
-   
-
 };
 
 int main()
-
  {
-
     Con c;
-
     c.show();
-
    return 0;
-
 } */
 
 
-
 /* #include<bits/stdc++.h>
-
 using namespace std;
-
 class Con 
-
 {
-
    int i=10;
-
    public:
-
    void show(){
-
     i++;                               //works
-
     cout<<i<<endl;
-
    }
-
-   
-
 };
 
 int main()
 
  {
-
     Con c;
-
     c.show();
-
    return 0;
-
 } */
