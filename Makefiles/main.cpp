4c1739e6a8a1f1c1ac577020e412d72ff018b21f
Anish Chandgothia <90914492+anish2910@users.noreply.github.com>
2022-09-06 12:34:13 +0530

    Create main.cpp

---
+++ Makefiles/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"functions.h"
+
+using namespace std;
+
+int main()
+
+{
+print_hello();
+cout<<endl;
+cout<<"maple"<<endl;
+cout<<"hi  let's learn makefile "<<endl;
+cout<<"factorial of 5 is "<<factorial(5)<<endl;
+
+return 0;
+
+}
+
